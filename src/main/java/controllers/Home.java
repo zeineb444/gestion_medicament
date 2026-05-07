3e75c8f865277f296b755d050fbd88fdf4c8c1ee
zeineb444 <zeineb.benali@esprit.tn>
2026-05-07 02:08:13 +0100

    Ajout interface JavaFX

---
+++ src/main/java/controllers/Home.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Home extends Application {
+
+    public static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) {
+        primaryStage = stage;
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/AjouterMedicament.fxml"));
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Gestion Médicament");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erreur de lecture : " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
